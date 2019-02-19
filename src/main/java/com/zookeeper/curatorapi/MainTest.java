a372813e4422228f762005e30e6d8716863967cb
yjshi2015 <495054021@qq.com>
2019-02-20 07:49:28 +0800

    add change

--- src/main/java/com/zookeeper/curatorapi/MainTest.java
+++ src/main/java/com/zookeeper/curatorapi/MainTest.java
@@ -8,5 +8,14 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println(Integer.MAX_VALUE/1000/60);
+
+        String str = "BEA";
+        changde(str);
+        System.out.println("str:"+str);
+    }
+
+    private static void changde(String str) {
+        str = str.replace("A", "E");
+        str = str.toLowerCase();
     }
 }
